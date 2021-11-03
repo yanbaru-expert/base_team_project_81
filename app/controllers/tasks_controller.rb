b85a2d3215469cc9642c5a7b4fc3bf1c67df294d
yuki <yukiiso0105@icloud.com>
2021-11-03 17:12:04 +0900

    新規投稿機能を実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,19 @@
 class TasksController < ApplicationController
+
+    def index
+        @tasks = Task.all
+    end
+
     def new
         @task = Task.new
+    end
+    
+    def create
+        Task.create(task_params)
+    end
+
+    private
+    def task_params
+        params.require(:task).permit(:title, :content)
     end
 end
