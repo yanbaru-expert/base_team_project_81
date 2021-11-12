6a845a7217dc1453004e089ba05efb6d610d8d26
Shota Nagayama <30305829kasukabe@gmail.com>
2021-11-12 15:40:44 +0900

    Prefixに変更、更新完了画面追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,4 @@
 class UsersController < ApplicationController
-  def index
-    @users = User.all
-  end
-
-  def show
-    @user = User.find(params[:id])
-  end
 
   def new
     @user = User.new
@@ -13,6 +6,14 @@ class UsersController < ApplicationController
 
   def create
     User.create(user_params)
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   def edit
